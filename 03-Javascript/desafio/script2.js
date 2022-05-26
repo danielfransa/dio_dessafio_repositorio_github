15a375300a077c025837e9b9bd060f76f8662fa2
danielfransa <daniel_fransa@hotmail.com>
2022-05-26 12:57:02 -0300

    atualização

--- 03-Javascript/desafio/script2.js
+++ 03-Javascript/desafio/script2.js
@@ -1,17 +1,32 @@
-const num1 = 1;
-const num2 = 1000;
+const A = parseInt(gets());
+const B = parseInt(gets());
+let soma = 0;
+
+n = B-A+1;
+const total = (n*(A+B))/2;
+console.log(total);
+
+
+/*
+const num1 = 1000;
+const num2 = 1;
+var num3 = 0;
 var total = 0;
 
+if (num1 > num2) {
+    num3 = num1;
+    for (let i = num2; i <= num3; i++) {
+        total = total + i;
+    }
+} else
+    for (let i = num1; i <= num2; i++) {
+        total = total + i;
+    }
 
-   for(let i = num1; i <= num2; i++) {
-     total = total + i;
-    
-   }
- 
-  console.log(total);
+console.log(total);
 
 
-/*const num1 = 1;
+const num1 = 1;
 const num2 = 1000;
 var total = 0;
 var contador = num1;
--- 03-Javascript/desafio/script5.js
+++ 03-Javascript/desafio/script5.js
@@ -1,3 +1,20 @@
+const numberOfMatches = parseInt(gets());
+let n = numberOfMatches;
+
+let totalMatches = 0;
+
+    while(n>1){
+        if(n%2==0){
+            totalMatches += n/2;
+                n = n/2
+        }else{
+            totalMatches +=(n-1)/2;
+                n = (n-1)/2+1
+        }
+    }
+    console.log(totalMatches)
+
+/*
 var squads = 14;             //parseInt(gets())
 var totalMatches = 0;
 var sobra = 0;
@@ -18,7 +35,7 @@ while (squads >= 2) {
     totalMatches = totalMatches + numberOfMatches;
 
 }
-console.log(totalMatches);
+console.log(totalMatches); */
 
 
 
